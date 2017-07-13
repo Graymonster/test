7ea2352f88f147863d9471612815ce77dccbe031
Graymonster <shakaka0329@gmail.com>
2017-07-14 08:20:51 +0900

    tt
    tt

--- common.js
+++ common.js
@@ -32,7 +32,7 @@ $.fn.disableScroll = function () {
     window.oldScrollPos = $(window).scrollTop();
     $(window).on('scroll.scrolldisabler', function (event) {
         $(window).scrollTop(window.oldScrollPos);
-        event.preventDefault();
+        event.preventDefault();aaaaa
     });
 };
 $.fn.enableScroll = function () {
